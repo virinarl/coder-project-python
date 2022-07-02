8e2cbbc131901622777c7145a40b08596423f01d
virinarl <v.irina.rod.lap@gmail.com>
2022-07-01 21:31:16 -0300

    primera view, función saludar, saludar_a con hora

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+
+def saludo(request):
+    return HttpResponse("Hola Mundo!")
+
+def saludar_a(request, nombre):
+    rightNow = datetime.now().strftime("%H:%M:%S") #Hora:minuto:segundos
+    return HttpResponse(f"Hola, {nombre.capitalize()}! Son las {rightNow}")
